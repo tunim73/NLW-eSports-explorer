f0ace638edebe6d0608153822e0c0f4d2ceb327a
tunim73 <antonio.netto973@gmail.com>
2022-09-19 23:59:26 -0300

    iniciando a version-react

---
+++ version-react/src/components/Home/index.jsx
@@ -0,0 +1,22 @@
+import { ImagemDePerfil } from '../ImagemDePerfil'
+import './home.css'
+
+export const Home = () => {
+  
+
+    return (
+      <div> 
+        
+        <header>
+          <ImagemDePerfil />
+        </header>
+        
+      </div>
+      
+    )
+}
+  
+  
+
+
+  
---
+++ version-react/src/components/ImagemDePerfil/index.jsx
@@ -0,0 +1,10 @@
+import "./imagemDePerfil.css"
+
+export const ImagemDePerfil = () => {
+    return(
+        <div className="div-perfil">
+            <img className = "imagem-de-pefil" src= "https://github.com/tunim73.png"  
+            alt="imagem de perfil"/>
+        </div>
+    )
+}
